a65a1d839c48cce2bed6c821b445bb03dab57a1d
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-07 10:15:34 +0900

    fix: refactor dev/prod path

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = knex(knexConfig.production);
-} else {
-  module.exports = knex(knexConfig.development);
-}
+const environment =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+module.exports = knex(knexConfig[environment]);
